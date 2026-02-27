6da4a8a4567b6b3180b26f2d46683d43f2be69f8
Ubuntu <ubuntu@ip-172-26-10-81.ap-northeast-1.compute.internal>
2026-02-27 03:17:36 +0000

    Force HTTPS behind gateway and trust proxy headers

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -27,8 +26,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        if (app()->environment('production')) {
-        URL::forceScheme('https');
-    }
     }
 }
